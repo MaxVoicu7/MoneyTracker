d25187d2fb41dd1acba5ddbbe9ec4a1d756e7fd8
MaxVoicu7 <mxmvoicu@gmail.com>
2024-05-10 14:23:46 +0300

    Created stages for login, menu and dashboard

--- src/main/java/com/moneyTracker/Controllers/LoginController.java
+++ src/main/java/com/moneyTracker/Controllers/LoginController.java
@@ -1,12 +1,18 @@
 package com.moneyTracker.Controllers;
 
+import com.moneyTracker.Models.Model;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
-public class LoginController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginController implements Initializable {
     @FXML
     private TextField emailTextField;
     @FXML
@@ -25,4 +31,15 @@ public class LoginController {
     private ChoiceBox<String> optionSelector;
     @FXML
     private Button optionButton;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        optionButton.setOnAction(event -> onLogin());
+    }
+
+    private void onLogin() {
+        Stage stage =(Stage) optionButton.getScene().getWindow();
+        Model.getInstance().getViewFactory().closeStage(stage);
+        Model.getInstance().getViewFactory().showUserWindow();
+    }
 }
--- src/main/java/com/moneyTracker/Views/ViewFactory.java
+++ src/main/java/com/moneyTracker/Views/ViewFactory.java
@@ -1,4 +1,58 @@
 package com.moneyTracker.Views;
 
+import com.moneyTracker.Controllers.UserController;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
 public class ViewFactory {
+    private AnchorPane dashboardView;
+
+    public ViewFactory() {
+
+    }
+
+    public AnchorPane getDashboardView() {
+        if (dashboardView == null) {
+            try {
+                dashboardView = new FXMLLoader(getClass().getResource("/Fxml/Dashboard.fxml")).load();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        return dashboardView;
+    }
+
+    public void showLoginWindow() {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/Fxml/Login.fxml"));
+        createStage(loader);
+    }
+
+    public void showUserWindow() {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/Fxml/User.fxml"));
+        UserController userController = new UserController();
+        loader.setController(userController);
+        createStage(loader);
+    }
+
+    private void createStage(FXMLLoader loader) {
+        Scene scene = null;
+
+        try {
+            scene = new Scene(loader.load());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.setTitle("Money Tracker");
+        stage.show();
+    }
+
+    public void closeStage(Stage stage) {
+        stage.close();
+    }
 }
